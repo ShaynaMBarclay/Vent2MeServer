c14417f89b8bcbaf66c99999c9fc3e40075891d3
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-05-05 13:32:44 -0400

    connected backend

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.set('trust proxy', true); // To get real IP if behind proxy
